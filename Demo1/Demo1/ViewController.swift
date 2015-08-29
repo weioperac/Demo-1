7a2d191ce1211365af80ba7931ef3aacf85b35ee
Wei Chen <weichenopera@gmail.com>
2015-08-29 18:50:09 +0800

    Hello Xcode!

---
+++ Demo1/Demo1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by Wei Chen on 15/8/29.
+//  Copyright (c) 2015年 Wei Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
